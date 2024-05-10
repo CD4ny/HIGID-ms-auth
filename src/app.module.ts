c8c28c780908d9be322215a6f1566b557956088e
daniel chaviano perez <danielcp@estudiantes.uci.cu>
2024-05-10 13:26:01 +0800

    feat: initial

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot({ isGlobal: true }),  UsersModule, AuthModule,]
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,45 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
+import {DocumentBuilder, SwaggerCustomOptions, SwaggerModule,} from '@nestjs/swagger';
+import 'dotenv/config';
+// @ts-ignore
+import {SwaggerTheme, SwaggerThemeNameEnum} from 'swagger-themes';
+import {BadRequestException, ValidationPipe} from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+  const apiPath: string = 'auth';
+  app.setGlobalPrefix(apiPath);
+  if (process.env.SWAGGER !== 'false') {
+    const options = new DocumentBuilder()
+        .addBearerAuth()
+        .setTitle('HIGID')
+        .setDescription('')
+        .setVersion('1.0')
+        .build();
+
+    const theme = new SwaggerTheme();
+    const customOptions: SwaggerCustomOptions = {
+      customSiteTitle: 'Api Docs',
+      swaggerOptions: {
+        jsonEditor: true,
+      },
+      customCss: theme.getBuffer(SwaggerThemeNameEnum.DARK),
+    };
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('api/docs', app, document, customOptions);
+  }
+
+  app.useGlobalPipes(
+      new ValidationPipe({
+        exceptionFactory: (errors) => {
+          console.error('Validation errors:', JSON.stringify(errors));
+          return new BadRequestException(errors);
+        },
+      }),
+  );
+  await app.listen(process.env.PORT || 5000);
 }
-bootstrap();
+
+bootstrap().then(r => console.log('Server is running on http://localhost:' + process.env.PORT || 5000));
